f03622c46d4a076229bcbffe69882426e95f3115
Gregory <gabrielgrigor26@gmail.com>
2026-07-02 21:17:29 +0500

    created patch endpoint

--- app/services/question_service.py
+++ app/services/question_service.py
@@ -1,12 +1,13 @@
 from fastapi import HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
-from ..schemas.question import QuestionCreate
-from ..schemas.answer import AnswerCreate
+from ..schemas.question import QuestionCreate, QuestionUpdate
+from ..schemas.answer import AnswerCreate, AnswerUpdate
 from ..models.question import Question
 from ..models.answer import Answer
 from typing import Sequence
-from ..crud.question import read_question, read_questions, create_question
-from ..crud.answer import create_answer 
+from ..crud.question import read_question, read_questions, create_question, update_question
+from ..crud.answer import create_answer, update_answer
+
 
 class QuestionService:
     def __init__(self, db: AsyncSession):
@@ -14,27 +15,27 @@ class QuestionService:
 
     async def create_question(self, question: QuestionCreate, user_id: int) -> Question:
         return await create_question(self.db, user_id, question)
-    
+
     async def read_question(self, question_id: int) -> Question | None:
-        question =  await read_question(self.db, question_id)
+        question = await read_question(self.db, question_id)
         if question is None:
             raise HTTPException(404, "Question not found!!!")
         return question
-    
+
     async def get_all_questions(self) -> Sequence[Question] | None:
         return await read_questions(self.db)
-    
+
     async def create_answer(self, question_id: int, answer_create: AnswerCreate, user_id: int) -> Answer:
-        return await create_answer(self.db, answer_create,question_id,user_id)
-    
+        return await create_answer(self.db, answer_create, question_id, user_id)
+
     async def delete_question(self):
         pass
 
-    async def edit_question(self):
-        pass
+    async def edit_question(self, question: QuestionUpdate, user_id: int, question_id: int):
+        return await update_question(self.db, question, user_id, question_id)
 
-    async def edit_answer(self):
-        pass
+    async def edit_answer(self, answer_id: int, answer: AnswerUpdate, user_id: int):
+        return await update_answer(self.db, answer, user_id, answer_id)
 
     async def delete_answer(self):
         pass
